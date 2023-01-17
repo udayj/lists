7fb776b0e6dc1a0fc587e54d777b5c0faaec04b6
root <jhunjhunwala.uday@gmail.com>
2023-01-17 13:07:19 +0000

    basic list functions for simple list

--- src/simple.rs
+++ src/simple.rs
@@ -8,3 +8,38 @@ struct Node <T> {
 pub struct List<T> {
     head: Link<T>
 }
+
+//push, pop, peek, add after i'th element
+
+impl <T> List <T> {
+
+    pub fn new() -> Self {
+
+        List {head: None}
+    }
+
+    pub fn push(&mut self, elem: T) {
+
+        let new_node = Box::new(Node {
+            elem: elem,
+            next: self.head.take()
+        });
+
+        self.head = Some(new_node)
+    }
+
+    pub fn pop(&mut self) -> Option<T> {
+
+        match self.head.take() {
+
+            Some(node) => {
+                self.head = node.next;
+                Some(node.elem)
+            }
+            None => {
+                self.head = None;
+                None
+            }
+        }
+    }
+}
