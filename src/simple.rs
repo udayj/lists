b9d1341230b12ef1fc15d800e382e388226064f4
root <jhunjhunwala.uday@gmail.com>
2023-01-18 16:08:17 +0000

    peek at an element at an index

--- src/simple.rs
+++ src/simple.rs
@@ -42,4 +42,43 @@ impl <T> List <T> {
             }
         }
     }
+ 
+    pub fn elem_at(&self, index: u32) -> Option<&T>{
+
+        let mut counter = 0;
+        let mut node = &self.head;
+        while counter<index {
+
+            match node {
+                Some(actual_node) => {
+                    counter+=1;
+                    node=&actual_node.next;
+                }
+                None => {
+                    return None;
+                }
+            }
+
+        }
+        //Some(&(node.as_ref().unwrap_or(None).elem))
+        node.as_ref().map(|actual_node| &actual_node.elem)
+    }
+}
+
+#[cfg(test)]
+mod tests
+{
+    use super::List;
+
+    #[test]
+    fn basics() {
+        let mut x = List::new();
+        x.push(1);
+        x.push(2);
+        x.push(3);
+        assert_eq!(x.elem_at(1),Some(&2));
+        assert_eq!(x.elem_at(0),Some(&3));
+        assert_eq!(x.elem_at(2),Some(&1));
+        assert_eq!(x.elem_at(3),None);
+    }
 }
